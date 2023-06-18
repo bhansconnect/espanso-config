9fa9310a9921abf1876e5462c11724b6acc21a9a
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2023-06-17 18:43:04 -0700

    add checking for duplicate triggers across all dictionaries

--- match/gen_dict.py
+++ match/gen_dict.py
@@ -3,6 +3,7 @@
 import csv
 import sys
 
+from glob import glob
 from os import path
 from pathlib import Path
 
@@ -19,24 +20,36 @@ if not path.exists(input_path):
     print(f'Could not find dictionary: {input_path}')
     exit(1)
 
+# Load all other dictionaries to ensure no collisions.
+triggers = {}
+for dict_path in script_path.parent.glob('*.csv'):
+    if path.normpath(dict_path) == path.normpath(input_path):
+        continue
+
+    with open(dict_path) as dict_file:
+        dict_csv = csv.reader(dict_file, delimiter=',')
+        for row in dict_csv:
+            # For these, just skip anything malformed
+            if len(row) != 2:
+                continue
+            triggers[row[0]] = row[1]
+
 out = '''
 #######  DO NOT EDIT   #######
 ####### AUTO GENERATED #######
 
 matches:\
 '''
-
 with open(input_path) as in_file:
     in_csv = csv.reader(in_file, delimiter=',')
-    triggers = set()
     for row in in_csv:
         if len(row) != 2:
             print(f'Malformed row: {row}')
             exit(1)
         if row[0] in triggers:
-            print(f'Trigger not unique: {row[0]}')
+            print(f'Trigger not unique: {row[0]} -> [{triggers[row[0]]}, {row[1]}]')
             exit(1)
-        triggers.add(row[0])
+        triggers[row[0]] = row[1]
         out += f'\n  - trigger: "{row[0]}"\n    replace: "{row[1]}"\n    propagate_case: true\n    word: true'
 
 output_path = input_path.with_suffix('.yml')
